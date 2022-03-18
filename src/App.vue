f7c2d6913e1576c7433b7de95537d4d597d04b13
dymafr <erwan1309@hotmail.com>
2022-03-18 13:25:00 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <div class="container">
+    <div>
+      <h3>Formulaire</h3>
+      <form>
+        <input type="text" placeholder="Prénom" />
+        <input type="text" placeholder="Email" />
+        <button>Sauvegarder</button>
+      </form>
+    </div>
+    <div>
+      <h3>Liste des utilisateurs</h3>
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts"></script>
 
