7ec376f7efea6b6f054ab727355b9a481599e29f
dymafr <erwan1309@hotmail.com>
2022-03-18 12:45:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-12d26k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-12d26k)
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template></template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+@import './assets/scss/base.scss';
+</style>
